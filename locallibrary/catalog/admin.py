d3d80742031d2ca80d0ab58ef70732674191d63c
Mason Myles <mason.myles98@gmail.com>
2024-04-02 17:50:56 -0500

    adding work to the admin page

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_birth')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+    def display_genre(self, obj):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in obj.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+
+# Register BookAdmin with the Book model
+admin.site.register(Book, BookAdmin)
